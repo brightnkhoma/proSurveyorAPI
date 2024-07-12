44f380b1ef38c9d7e08f2378256c19620e27b161
bright nkhoma <gis-020-20@must.ac.mw>
2024-07-13 00:39:38 +0200

    hosts allowed

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure-0bn@#fob08j^&^qu_sltc7-gq)_6plgrygg86e!%34vmj)ux7j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-     '10.0.2.2', 
-     '127.0.0.1' ,
-     '.vercel.app'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
