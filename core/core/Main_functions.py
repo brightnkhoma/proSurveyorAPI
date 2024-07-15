99d34c68ba9752ccf0b59f204beb8848e00d636f
bright nkhoma <gis-020-20@must.ac.mw>
2024-07-15 22:35:13 +0200

    testing interpolate method

--- core/core/Main_functions.py
+++ core/core/Main_functions.py
@@ -37,7 +37,7 @@ class Interpolate:
         plt.close()
         return {"answer": img_base64,"type" : "image"}
     
-    def interpolate( self,bins : int, email:str, data : list, shapefile_path : str,contour_map : bool = True,crs : int = 4326, report :bool = False,title : str = "",model : str = "exp"):
+    def interpolate( self,bins : int, data : list, shapefile_path : str,contour_map : bool = True,crs : int = 4326, report :bool = False,title : str = "",model : str = "exp",email:str = "test@gmail.com"):
         data = np.array(data)
         latitudes = data[:,0]
         longitudes = data[:,1]
