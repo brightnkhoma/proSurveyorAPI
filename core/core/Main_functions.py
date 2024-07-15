287ee4745c7be19d7ebe67cb965b344b989e822a
bright nkhoma <gis-020-20@must.ac.mw>
2024-07-15 22:16:54 +0200

    update interpolate method

--- core/core/Main_functions.py
+++ core/core/Main_functions.py
@@ -9,8 +9,12 @@ import matplotlib
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import requests
+import zipfile
+
 matplotlib.use("agg")
 load_dotenv()
+
 
 class Interpolate:
     def __init__(self) -> None:
@@ -33,7 +37,7 @@ class Interpolate:
         plt.close()
         return {"answer": img_base64,"type" : "image"}
     
-    def interpolate( self,bins : int,data : list, shapefile_path : str,contour_map : bool = True,crs : int = 4326, report :bool = False,title : str = "",model : str = "exp"):
+    def interpolate( self,bins : int, email:str, data : list, shapefile_path : str,contour_map : bool = True,crs : int = 4326, report :bool = False,title : str = "",model : str = "exp"):
         data = np.array(data)
         latitudes = data[:,0]
         longitudes = data[:,1]
@@ -42,13 +46,13 @@ class Interpolate:
         x_input = np.column_stack([longitudes,latitudes])
         krigging_model = SimpleKriging()
         krigging_model.load(X=x_input,y=values)
-        krigging_model.variogram(bins=bins)
+        krigging_model.variogram(bins=2)
         krigging_model.fit(model=model)
         z = []
         z.append(self.convert())
         plt.savefig("zz.png")
         
-        initial_geoDataFrame = self.get_shapeFile(shape_file_path=shapefile_path)
+        initial_geoDataFrame = self.get_shapeFile(self.downdload_shapefile(shapefile_path,f"shapefiles/{email}"))
         final_geoDataFrame = initial_geoDataFrame.to_crs(epsg=crs)
 
         transformer = TransformerGDF()
@@ -70,8 +74,12 @@ class Interpolate:
              fig.colorbar(cbar)   
         z.append(self.convert())
         if(report):
-            report = self.createReport(title,krigging_model.evaluate(return_=True))
+            print("222222222222\n\n\n\n\n\\n\n\n\n\n\n\n")
+            stats = krigging_model.evaluate(groups=bins, return_=True)
+            report = self.createReport(title,stats=stats)
+            print(333)
             plt.close()
+            print(report)
             return {"array":z, "report" : report}
         plt.close()
         return {"array":z, "report" : ""}
@@ -79,14 +87,31 @@ class Interpolate:
 
     def createReport(self,title : str, stats : str)->str:
         try:
+            print("starting report")
             api_key = os.getenv("GEMINI_API_KEY")
+            print(api_key, " key")
             genai.configure(api_key= api_key)
             model = genai.GenerativeModel("gemini-1.5-flash")
             response = model.generate_content(f"write a field report about {title} with these stats : {stats}")
             return f"{response.text}"
         except Exception:
             print(Exception)
-            return "Failed to generate"
+           # return "Failed to generate"
+    def downdload_shapefile(self,path : str, destination : str):
+        response = requests.get(path)
+        if response.status_code == 200 :
+            with zipfile.ZipFile(io.BytesIO(response.content)) as z:
+                z.extract(destination)
+                return self.find_shapefiles(destination)
+
+
+    def find_shapefiles(self,directory : str):
+        shp_files = []
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith('.shp'):
+                    shp_files.append(os.path.join(root, file))
+        return shp_files
         
         
 
--- core/core/views.py
+++ core/core/views.py
@@ -9,7 +9,8 @@ def krigging(request):
     data = request.GET.get("data")    
     data = json.loads(data)
     Interpolator = Main_functions.Interpolate()
-    result = Interpolator.interpolate(data=data['array'],shapefile_path=r'C:\Users\blown\OneDrive\Desktop\Malawi Data\mw_districts_pop_2008_new.shp',report=data['report'],title=data['title'],model=data['model'],bins = data["neighbors"])  
+    print(data)
+    result = Interpolator.interpolate(data=data['array'],shapefile_path=r'C:\Users\blown\OneDrive\Desktop\Malawi Data\mw_districts_pop_2008_new.shp',report=data['report'],title=data['title'],model=data['model'],bins = 2)  
     content = result["array"]
     content.append({"answer" : result["report"],"type": "report"})    
     return JsonResponse(content, safe=False)
