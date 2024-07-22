ef416514ab3fe66e9ed00df5ddb0bf1b9ddbdc45
bright nkhoma <gis-020-20@must.ac.mw>
2024-07-22 14:41:25 +0200

    handle exceptions

--- core/core/views.py
+++ core/core/views.py
@@ -6,13 +6,17 @@ import json
 
 @require_GET
 def krigging(request):
-    data = request.GET.get("data")    
-    data = json.loads(data)
-    Interpolator = Main_functions.Interpolate()    
-    result = Interpolator.interpolate(data=data['array'],shapefile_path=r'https://firebasestorage.googleapis.com/v0/b/wews-7fc10.appspot.com/o/files%2Fblownbrian%40gmail.com%2FstudyArea.zip?alt=media&token=f0b4865d-9ef1-4e5f-b12f-7972a4271aa4',report=data['report'],title=data['title'],model=data['model'],bins = 2)  
-    content = result["array"]
-    content.append({"answer" : result["report"],"type": "report"})    
-    return JsonResponse(content, safe=False)
+    try:
+        data = request.GET.get("data")    
+        data = json.loads(data)
+        Interpolator = Main_functions.Interpolate()    
+        result = Interpolator.interpolate(data=data['array'],shapefile_path=r'https://firebasestorage.googleapis.com/v0/b/wews-7fc10.appspot.com/o/files%2Fblownbrian%40gmail.com%2FstudyArea.zip?alt=media&token=f0b4865d-9ef1-4e5f-b12f-7972a4271aa4',report=data['report'],title=data['title'],model=data['model'],bins = 2)  
+        content = result["array"]
+        content.append({"answer" : result["report"],"type": "report"})    
+        return JsonResponse(content, safe=False)
+    except Exception as e :
+        return JsonResponse([{"answer" : "something went wrong","type" : "report"}])
+        
 
 
 @require_GET
